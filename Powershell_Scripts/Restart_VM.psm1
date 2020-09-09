3f0fa3c4a145d909f81891558fc3fcaded58ca89
anuragpatle <anurag.ap6@gmail.com>
2020-09-09 04:20:59 +0000

    Fixed some bugs

--- Powershell_Scripts/Restart_VM.psm1
+++ Powershell_Scripts/Restart_VM.psm1
@@ -1,10 +1,30 @@
-function RestartVM ($VMName, $RGName) {
-    Restart-AzureRmVM -ResourceGroupName $RGName -Name $VMName
+function RestartVM ($ResourceId) {
+    $Resource = Get-AzureRmResource -ResourceId $ResourceId
+
+    Restart-AzureRmVM -ResourceGroupName $Resource.ResourceGroupName -Name $Resource.Name 
 }
 
-function StopVM ($VMName, $RGName) {
-    Stop-AzureRmVM -ResourceGroupName $RGName -Name $VMName -Force
+function StartVM ($ResourceId) {
+    $Resource = Get-AzureRmResource -ResourceId $ResourceId
+
+    Start-AzureRmVM -ResourceGroupName $Resource.ResourceGroupName -Name $Resource.Name 
 }
+
+# function StopVM ($VMName, $RGName) {
+#     Stop-AzureRmVM -ResourceGroupName $RGName -Name $VMName -Force
+# }
+function StopVM ($ResourceId) {
+    
+    $Resource = Get-AzureRmResource -ResourceId $ResourceId
+    
+    Write-Output "PS: Called StopVM for resource id: " $Resource.Name
+
+    Stop-AzureRmVM -ResourceGroupName $Resource.ResourceGroupName -Name $Resource.Name -Force
+
+}
+
+
 
 Export-ModuleMember -Function RestartVM
 Export-ModuleMember -Function StopVM
+Export-ModuleMember -Function StartVM
--- app.js
+++ app.js
@@ -6,47 +6,66 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 
-function executePS() {
+function stopVM(resourceId) {
+
   const ps = new Shell({
     executionPolicy: 'Bypass',
     noProfile: true
   });
-   
+
   ps.addCommand(' Get-Location');
   ps.addCommand('cd Powershell_Scripts');
   ps.addCommand('.\\Main.ps1');
-  ps.addCommand("StopVM 'NodeServer' 'TSI-TEST-RG'");
+  ps.addCommand("StopVM " + resourceId);
+  // ps.addCommand("ExucuteCreateVM");
   ps.addCommand(' Get-Location');
+
   ps.invoke()
-  .then(output => {
-    console.log(output);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then(output => {
+      console.log(output);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 app.get("/", function (req, res) {
-  res.send("Hello world!");
- executePS() 
+  res.send("You made a GET Request to the NodePS-Srvr2 !");
 });
 
 app.post("/hi", function (req, res) {
+
   var body = req.body;
   console.log(body);
 
   try {
-    
-    console.log(body.data.alertContext.condition)
-    
-    console.log(body.data.essentials.alertTargetIDs)
-    console.log(body.data.alertContext.condition.allOf[0].dimensions)
+
+    // console.log("Printing Condition: " + body.data.alertContext.condition)
+
+    // console.log("Printing alertTargetIDs: " + body.data.essentials.alertTargetIDs)
+    let firstVM = body.data.essentials.alertTargetIDs[0]
+    // console.log("Printing alertTargetIDs[0]: " + firstVM)
+    // console.log("Printing allOf[0].dimensions: " + body.data.alertContext.condition.allOf[0].dimensions)
+
+    // let metricName = body.data.alertContext.condition.allOf[0].metricName // 'Percentage CPU'
+    // console.log("metric name: " + metricName)
+    let resourceIds = body.data.essentials.alertTargetIDs
+    // stopVM(firstVM)
+
+    body.data.essentials.alertTargetIDs.forEach(resName => {
+      console.log("resource name is: " + resName)
+      stopVM(resName)
+    });
+
+
+  
+
 
   } catch (err) {
     console.error(err);
   }
 
-  res.send("user added!");
+  res.send("You made a POST Request to the NodePS-Srvr2 !");
 });
 
 app.listen(3000, function () {
@@ -59,17 +78,16 @@ app.listen(3000, function () {
   schemaId: 'azureMonitorCommonAlertSchema',
   data: {
     essentials: {
-      alertId: '/subscriptions/8efc1d1b-ff8c-4646-8b64-027cd8f131fc/providers/Microsoft.AlertsManagement/alerts/d8caa4ba-4c03-4e7f-9eb2-53f42c21e10c',
-      alertRule: 'webhooktest',
-      severity: 'Sev4',
+      alertId: '/subscriptions/8efc1d1b-ff8c-4646-8b64-027cd8f131fc/providers/Microsoft.AlertsManagement/alerts/cdbee921-16dd-4580-9c89-a421f276f09a',
+      alertRule: 'VM26_alert',
+      severity: 'Sev3',
       signalType: 'Metric',
-      monitorCondition: 'Resolved',
+      monitorCondition: 'Fired',
       monitoringService: 'Platform',
       alertTargetIDs: [Array],
-      originAlertId: '8efc1d1b-ff8c-4646-8b64-027cd8f131fc_TSI-Test-RG_microsoft.insights_metricAlerts_webhooktest_-1302684072',
-      firedDateTime: '2020-09-02T12:34:17.5011792Z',
-      resolvedDateTime: '2020-09-02T12:42:30.4414967Z',
-      description: '',
+      originAlertId: '8efc1d1b-ff8c-4646-8b64-027cd8f131fc_TSI-Test-RG_microsoft.insights_metricAlerts_VM26_alert_869486483',
+      firedDateTime: '2020-09-08T05:56:29.1563179Z',
+      description: 'some desc',
       essentialsVersion: '1.0',
       alertContextVersion: '1.0'
     },
@@ -87,15 +105,45 @@ app.listen(3000, function () {
       metricName: 'Percentage CPU',
       metricNamespace: 'Microsoft.Compute/virtualMachines',
       operator: 'GreaterThan',
-      threshold: '1',
+      threshold: '2',
       timeAggregation: 'Maximum',
       dimensions: [Array],
-      metricValue: 0.52,
+      metricValue: 22.99,
+      webTestName: null
+    },
+    {
+      metricName: 'Percentage CPU',
+      metricNamespace: 'Microsoft.Compute/virtualMachines',
+      operator: 'GreaterThan',
+      threshold: '5',
+      timeAggregation: 'Count',
+      dimensions: [Array],
+      metricValue: 7,
       webTestName: null
     }
   ],
-  windowStartTime: '2020-09-02T12:34:07.22Z',
-  windowEndTime: '2020-09-02T12:39:07.22Z'
+  windowStartTime: '2020-09-08T05:48:14.295Z',
+  windowEndTime: '2020-09-08T05:53:14.295Z'
 }
+
+
+
+[
+  '/subscriptions/8efc1d1b-ff8c-4646-8b64-027cd8f131fc/resourcegroups/tsi-test-rg/providers/microsoft.compute/virtualmachines/d1vm26'
+]
+
+
+
+
+[
+  {
+    name: 'microsoft.resourceId',
+    value: '/subscriptions/8efc1d1b-ff8c-4646-8b64-027cd8f131fc/resourceGroups/TSI-Test-RG/providers/Microsoft.Compute/virtualMachines/D1VM26'
+  },
+  {
+    name: 'microsoft.resourceType',
+    value: 'Microsoft.Compute/virtualMachines'
+  }
+]
 
 */
